d877ad578ab5af33ab25e4e51585da1fd98aaeec
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-01-05 02:43:54 -0600

    implemented click interface on plantcam.py and reworked remote_run.py to use

--- plantcam.py
+++ plantcam.py
@@ -1,11 +1,16 @@
 from time import sleep
 from picamera import PiCamera
+import click
 
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-# camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.vflip = True
-camera.capture('foo.jpg')
-print('Done')
+@click.command()
+@click.option('--filename', '-f', type=str, default='foo.jpg')
+def take_picture(filename):
+    camera = PiCamera()
+    camera.resolution = (1920, 1080)
+    sleep(2)
+    camera.vflip = True
+    camera.capture(filename)
+    print('Done')
+
+if __name__ == '__main__':
+    take_picture()
--- remote_run.py
+++ remote_run.py
@@ -2,16 +2,24 @@ from pathlib import Path
 import ftp
 import paramiko
 
-def run_ssh(filename):
-    if not isinstance(filename, Path):
-        filename = Path.cwd() / filename
+def run_ssh(script_name, **kwargs):
+    if not isinstance(script_name, Path):
+        script_name = Path.cwd() / script_name
 
-    ftp.send_file(filename)
+    cmd = 'python3 '
+    if len(kwargs) > 0:
+        cmd += '{} '.format(script_name.name)
+        for arg in kwargs:
+            cmd += '--{} {}'.format(arg, kwargs[arg])
+    else:
+        cmd += script_name.name
+
+    ftp.send_file(script_name)
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(ftp.IP, username=ftp.USER, password=ftp.PASSWORD)
-        stdin, stdout, stderr = ssh.exec_command('python3 {}'.format(filename.name))
+        stdin, stdout, stderr = ssh.exec_command(cmd)
         result = stdout.read().decode('ascii').strip()
         error = stderr.read().decode('ascii').strip()
     finally:
@@ -24,5 +32,5 @@ def run_ssh(filename):
         print(result)
 
 if __name__ == '__main__':
-    run_ssh('plantcam.py')
+    run_ssh('plantcam.py', filename='test.png')
     ftp.get_pictures()
