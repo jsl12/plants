ba7a2e42ee895394153b126bbd31a5073f748251
jsl12 <lancaster.js@gmail.com>
2020-08-24 15:47:53 -0500

    added the ability to scale the timelapse output, intended for use with Discord

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,7 @@ from utils.file import filter_time
 from utils.sun import get_twilights
 
 
-def daybreak(source: Path, output_folder: Path, **kwargs):
+def day(source: Path, output_folder: Path, suffix: str = None, **kwargs):
     df = get_twilights()
     files = filter_time(
         source,
@@ -15,6 +15,6 @@ def daybreak(source: Path, output_folder: Path, **kwargs):
     )
     timelapse(
         files=files.to_list(),
-        output=output_folder / f'{datetime.now().strftime("%Y-%m-%d")}_daybreak.mp4',
+        output=output_folder / f'{datetime.now().strftime("%Y-%m-%d")}_{suffix or "daybreak"}.mp4',
         **kwargs
     )
--- utils/animate.py
+++ utils/animate.py
@@ -10,7 +10,7 @@ def write_filelist(path: Path, filepaths):
         file.writelines([f'file {f.as_posix()}\n' for f in filepaths])
 
 
-def timelapse(files, output, framerate = None, overwrite: bool = True, colors=False):
+def timelapse(files, output, framerate = None, overwrite: bool = True, colors=False, scale: str = None):
     # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
     # https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
     LOGGER.info(f'Concatenating {len(files)} files into {output}')
@@ -21,8 +21,11 @@ def timelapse(files, output, framerate = None, overwrite: bool = True, colors=Fa
     temp_filelist = files[0].with_name('filelist.txt')
     write_filelist(temp_filelist, sorted(files, key=lambda f:f.name))
 
+    input = ffmpeg.input(temp_filelist, format='concat', safe=0)
+    if scale is not None:
+        input = input.filter('scale', scale, force_original_aspect_ratio='decrease')
     (
-        ffmpeg.input(temp_filelist, format='concat', safe=0)
+        input
         .output(output.with_suffix('.mp4').as_posix(), **output_kwargs)
         .run(overwrite_output=overwrite)
     )
