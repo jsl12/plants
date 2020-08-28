a9243894e464d04ce1b85f8e8a2ee8e2a1be9534
jsl12 <lancaster.js@gmail.com>
2020-08-27 23:59:56 -0500

    turned into a click interface

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
+import logging
 from datetime import datetime
 from pathlib import Path
 from time import sleep
+from typing import Tuple
 
+import click
 import pandas as pd
 from picamera import PiCamera
 from pvlib import solarposition
 
-if __name__ == '__main__':
+LOGGER = logging.getLogger(__name__)
+
+@click.command()
+@click.option('--sensor_mode', '-m', type=int, default=0)
+@click.option('--resolution', '-res', type=(int, int), default=(None, None))
+@click.option('--res_folder', '-rf', type=click.Path(exists=True), default='/mnt/nas/')
+@click.option('--name', '-n', type=str)
+@click.option('--text_size', '-ts', type=int, default=None)
+@click.option('--min_elevation', '-e', type=int, default=-18)
+def take_picture(
+        sensor_mode: int,
+        resolution: Tuple[int],
+        res_folder: str,
+        name: str,
+        text_size: int,
+        min_elevation:int
+):
     pos = solarposition.get_solarposition(
         pd.DatetimeIndex([datetime.now()], tz='US/Central'),
         30.250657,
         -97.748108
     ).iloc[0]
-    pos_str = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\nElevation: {pos.elevation:.2f}, Azimuth: {pos.azimuth:.2f}'
 
-    if pos['elevation'] >= -18:
-        print(pos_str)
-        cam = PiCamera(sensor_mode=1, resolution=(1920, 1080))
-        cam.annotate_text = pos_str
-        cam.annotate_text_size = 20
-        sleep(2)
-        res_file = (Path(r'/mnt/nas/') / f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_chives.jpg')
-        cam.capture(res_file.as_posix())
-        print(f'Captured to {res_file.name}')
-        cam.close()
+    try:
+        cam = PiCamera(sensor_mode=sensor_mode, resolution=None if resolution[0] is None else resolution)
+    except Exception as e:
+        LOGGER.exception(e)
+        return
     else:
-        print(f'Skipping, {pos_str}')
+        LOGGER.info(f'Sleeping for initialization...')
+        sleep(2)
+        LOGGER.info(f'PiCamera initialized: Mode {cam.sensor_mode} - {cam.resolution[0]}x{cam.resolution[1]}')
+
+    pic_date = datetime.now()
+
+    if text_size is not None:
+        cam.annotate_text = f'{pic_date.strftime("%Y-%m-%d %H:%M:%S")}, Elevation: {pos.apparent_elevation:.1f}, Azimuth: {pos.azimuth:.1f}'
+        cam.annotate_text_size = text_size
+
+    res_file = Path(res_folder) / f'{pic_date.strftime("%Y-%m-%d_%H-%M-%S")}_{name}.jpg'
+
+    if pos.apparent_elevation >= min_elevation:
+        try:
+            cam.capture(res_file.as_posix())
+        except Exception as e:
+            LOGGER.exception(e)
+            return
+        else:
+            LOGGER.info(f'Captured to {res_file.name}')
+        finally:
+            cam.close()
+    else:
+        LOGGER.info(pic_date)
+        LOGGER.info(f'Apparent elevation too low: {pos.apparent_elevation:.2f}')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    take_picture()
--- utils/__init__.py
+++ utils/__init__.py
@@ -7,13 +7,14 @@ from .file import filter_time
 from .sun import get_twilights
 
 
-def day(source: Path, output_folder: Path, suffix: str = None, **kwargs):
+def day(source: Path, output_folder: Path, suffix: str = None, downsample: int = None, **kwargs):
     df = get_twilights()
     files = filter_time(
         source,
         start=df.loc['Civil Twilight', 'Begin'].iloc[0],
         end=df.loc['Civil Twilight', 'End'].iloc[1]
     )
+    files = files.iloc[::downsample]
     timelapse(
         files=files.to_list(),
         output=output_folder / f'{datetime.now().strftime("%Y-%m-%d")}_{suffix or "daybreak"}.mp4',
