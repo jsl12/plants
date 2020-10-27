8a7bb32cc8b477f6a874d0593be4bafc9b4a645c
jsl12 <lancaster.js@gmail.com>
2020-10-26 23:22:15 -0500

    converted to threaded multiple temperature readings

--- pi/temp.py
+++ pi/temp.py
@@ -1,10 +1,13 @@
 import logging
+import random
 import sqlite3
+import threading
 import time
 from datetime import datetime
 from pathlib import Path
 from typing import List
 
+import numpy as np
 import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
@@ -53,10 +56,46 @@ def sql_conn(db_file: Path)-> sqlite3.Connection:
         return conn
 
 
-def record_temp(db_file: Path, table_name: str = 'temp_readings', create_table: bool = False):
-    conn = sql_conn(db_file)
+def read_temp_df(db_file: Path, table_name: str = 'temp_readings') -> pd.DataFrame:
+    with sqlite3.connect(db_file) as conn:
+        df = pd.read_sql(f'select * from {table_name}', conn, index_col='time', parse_dates='time')
+    conn.close()
+    return df
+
+
+def record_temps(num_reads: int, timing: float, db_file: Path, table_name: str, create_table: bool = False):
+    data = np.full(num_reads, 0.0)
+    times = np.full(num_reads, datetime.now())
+    lock = threading.Lock()
+
+    def read(i):
+        try:
+            with lock:
+                data[i], times[i] = random.random(), datetime.now()
+                # data[i], times[i] = read_temp(), datetime.now()
+        except Exception as e:
+            LOGGER.exception(e)
+        else:
+            LOGGER.info(f'{times[i].strftime("%H:%M:%S")} {data[i]:.1f}')
+
+    threads = [threading.Timer(i * timing, read, args=(i,)) for i in range(num_reads)]
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    with lock:
+        add_to_sql(db_file=db_file,
+                   times=times,
+                   data=data,
+                   table_name=table_name,
+                   create_table=create_table)
+
+
+def add_to_sql(db_file: Path, times: np.array, data: np.array, table_name: str, create_table: bool = False):
     try:
-        with conn:
+        with sql_conn(db_file) as conn:
             if create_table:
                 create_table_command = f"""CREATE TABLE IF NOT EXISTS {table_name} (
                                             time datetime PRIMARY KEY,
@@ -65,30 +104,27 @@ def record_temp(db_file: Path, table_name: str = 'temp_readings', create_table: 
                 conn.execute(create_table_command)
                 LOGGER.info(f'CREATE TABLE IF NOT EXISTS {table_name}')
 
-            insert_value_command = f'INSERT INTO {table_name}(time,temp) VALUES(?,?);'
-            reading = (datetime.now(), read_temp())
-            conn.execute(insert_value_command, reading)
+            conn.executemany(f'INSERT INTO {table_name}(time,temp) VALUES(?,?);', zip(times, data))
     except sqlite3.Error as e:
         raise e
     else:
-        LOGGER.info(f'Logged {reading[0].strftime("%Y-%m-%d %H:%M:%S")} {reading[1]:.1f} C')
+        LOGGER.info(f'Added {times.shape[0]} readings, average: {data.mean():.1f}C')
     finally:
         conn.close()
         LOGGER.info(f'Closed connection to {db_file.as_posix()}')
 
 
-def read_temp_df(db_file: Path, table_name: str = 'temp_readings') -> pd.DataFrame:
-    with sqlite3.connect(db_file) as conn:
-        df = pd.read_sql(f'select * from {table_name}', conn, index_col='time', parse_dates='time')
-    conn.close()
-    return df
-
-
 if __name__ == '__main__':
-    print('starting')
+    print('Starting')
     logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                         format='%(asctime)-15s [%(levelname)s] %(module)s: %(message)s')
-    logging.info(f'{read_temp():.1f} C')
     p = Path('/home/pi/Documents/plants/temps.db')
-    record_temp(p)
-    print('done')
+    record_temps(
+        num_reads=30,
+        timing=1.0,
+        db_file=p,
+        # db_file=Path(r'C:\Users\lanca\OneDrive\Documents\Software\Plants\temp\temps.db'),
+        # table_name='test',
+        # create_table=True
+    )
+    print('Done')
